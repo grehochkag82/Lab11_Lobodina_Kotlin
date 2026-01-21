d4abc1f798e792a696c2d3f471c44a43cbdc9ed5
grehochka82 <lobodina0607@mail.ru>
2026-01-21 13:06:47 +0300

    learn sealed-class

--- src/example/SealedClass.kt
+++ src/example/SealedClass.kt
@@ -67,6 +67,20 @@ object TrafficController{
         carCount++
     }
 }
+sealed class OrderStatus{
+    object Created : OrderStatus()
+    object Paid: OrderStatus()
+    object Shipped: OrderStatus()
+    data class Cancelled(val reason: String): OrderStatus()
+}
+fun handleOrder(status: OrderStatus){
+    when(status){
+        OrderStatus.Created-> println("Заказ создан")
+        OrderStatus.Paid -> println("Заказ оплачен")
+        OrderStatus.Shipped -> println("Заказ отправлен")
+        is OrderStatus.Cancelled-> println("Отменён: ${status.reason}")
+    }
+}
 fun main(){
     val success = NetworkResult.Success("Данные получены")
     val error = NetworkResult.Error("Сервер не отвечает",500)
@@ -99,4 +113,9 @@ fun main(){
     MyCar("Toyota")
     MyCar("BMW")
     TrafficController.carPassed()
+    handleOrder(OrderStatus.Created)
+    handleOrder(OrderStatus.Paid)
+    handleOrder(OrderStatus.Shipped)
+    handleOrder(OrderStatus.Cancelled("Нет товара на складе"))
+
 }
