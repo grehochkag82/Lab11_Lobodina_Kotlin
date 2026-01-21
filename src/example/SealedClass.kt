2891b8f7db843149b4a908d10362c829784ba990
grehochka82 <lobodina0607@mail.ru>
2026-01-21 12:06:22 +0300

    learn Object

--- src/example/SealedClass.kt
+++ src/example/SealedClass.kt
@@ -19,6 +19,54 @@ fun handleResult(result: NetworkResult){
         }
     }
 }
+object GameSession{
+    init {
+        println("Игровая сессия создана")
+    }
+    var isActive: Boolean = false
+    fun start(){
+        isActive= true
+        println("Игра началась")
+    }
+    fun end(){
+        isActive = false
+        println("Игра завершена")
+    }
+}
+
+object Logger {
+    var count =0
+    fun log (message: String){
+        count++
+        println("[$count] $message")
+    }
+}
+object  AppSettings{
+    val version= "1.0.0"
+    var isDarkMode = true
+    fun toggleTheme(){
+        isDarkMode= !isDarkMode
+    }
+    fun checkTheme(){
+        if (AppSettings.isDarkMode){
+            println("Темная тема включена")
+        }
+    }
+}
+object  Colors{
+    const val RED= "#FF0000"
+    const val GREEN = "#00FF00"
+    const val BLUE = "0000FF"
+}
+class MyCar(val model: String) {
+    fun drive() = println("$model едет")
+}
+object TrafficController{
+    var carCount=0
+    fun carPassed(){
+        carCount++
+    }
+}
 fun main(){
     val success = NetworkResult.Success("Данные получены")
     val error = NetworkResult.Error("Сервер не отвечает",500)
@@ -26,4 +74,29 @@ fun main(){
     handleResult(success)
     handleResult(error)
     handleResult(loading)
+    println("Программв запущена")
+    println("Проверяем состояние, но не трогаем GameSession")
+    println("Теперь запускем игру")
+    GameSession.start()
+    println("Проверяем состояние ещё раз")
+    println("Активна ли сессия: ${GameSession.isActive}")
+    Logger.log("Первое сообщение")
+    Logger.log("Второе сообщение")
+    val logger1= Logger
+    val logger2= Logger
+    println(logger1==logger2)
+    println(Colors.RED)
+    println(Colors.GREEN)
+    println(Colors.BLUE)
+    val handler = object {
+        val name = "Обработчик"
+        fun handle() {
+            println("Обрабатываю...")
+        }
+    }
+    println(handler.name)
+    handler.handle()
+    MyCar("Toyota")
+    MyCar("BMW")
+    TrafficController.carPassed()
 }
